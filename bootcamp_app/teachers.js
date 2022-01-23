d9ae9878c317bc06c65316ad70c34b69ee5ea8e3
Hollie Hensman <hensmanh@gmail.com>
2022-01-23 22:39:19 +0000

    Add parameterized queries to students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const querySTring = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+`;
+
+
+
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
